1b6bcd446f638353674707f2fd5a12fa96ebf09b
Cian <cian.omondi@student.moringaschool.com>
2021-07-30 07:31:12 +0300

    initial commit

---
+++ cred.py
@@ -0,0 +1,18 @@
+ class Credentials:
+     """
+     Created user class that will store all the data upon login
+     """
+
+     pass
+
+def __init__(self,user_name,password):
+        """
+        Initialized an object with self and the parameters it will use.
+        
+        Args:
+        first_name: New user first name.
+        last_nameL New user last name.
+        user_name: New user first name.
+        password: New user password.
+        
+        """    
---
+++ user.py
@@ -0,0 +1,18 @@
+ class User:
+     """
+     Created user class that will store all the data upon login
+     """
+
+     pass
+
+def __init__(self,first_name,last_name,user_name,password):
+        """
+        Initialized an object with self and the parameters it will use.
+        
+        Args:
+        first_name: New user first name.
+        last_nameL New user last name.
+        user_name: New user first name.
+        password: New user password.
+        
+        """    
